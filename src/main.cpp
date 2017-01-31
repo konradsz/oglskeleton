088756d34155d050ef63fb8d6a73825b7aa5d9b5
konradsz <szymoniak.konrad@gmail.com>
2017-01-31 18:29:43 +0100

    creates empty window using glfw

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
+#include <iostream>
+
 #define GLEW_STATIC
 #include <GL/glew.h>
 
 #include <GLFW/glfw3.h>
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+const GLuint WINDOW_WIDTH = 800;
+const GLuint WINDOW_HEIGHT = 600;
 
 int main()
 {
@@ -11,5 +18,47 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "oglskeleton", nullptr, nullptr);
+    if (!window)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    int width;
+    int height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+
     return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
 }
