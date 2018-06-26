a460a212d91f42f79e36b97cdbf2748e1de19b2f
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2018-06-26 20:03:30 +0200

    extract GLFW function calls to separate class

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,12 @@
-#include <iostream>
-
-#define GLEW_STATIC
-#include <GL/glew.h>
-
-#include <GLFW/glfw3.h>
-
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
-
-const GLuint WINDOW_WIDTH = 800;
-const GLuint WINDOW_HEIGHT = 600;
+#include "ContextGLFW.hpp"
 
 int main() {
-    if (!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW" << std::endl;
-        return -1;
-    }
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-    glfwWindowHint(GLFW_REFRESH_RATE, 60);
+    const GLuint WINDOW_WIDTH = 800;
+    const GLuint WINDOW_HEIGHT = 600;
+    ogl::ContextGLFW context(WINDOW_WIDTH, WINDOW_HEIGHT);
+    context.print_info();
 
-    GLFWwindow* window =
-        glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "oglskeleton", nullptr, nullptr);
-    if (!window) {
-        std::cerr << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetKeyCallback(window, key_callback);
-
-    glewExperimental = GL_TRUE;
-    if (glewInit() != GLEW_OK) {
-        std::cerr << "Failed to initialize GLEW" << std::endl;
-        return -1;
-    }
-
-    int width;
-    int height;
-    glfwGetFramebufferSize(window, &width, &height);
-    glViewport(0, 0, width, height);
-
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwSwapBuffers(window);
-    }
-
-    glfwTerminate();
+    context.loop();
 
     return 0;
-}
-
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, GL_TRUE);
-    }
 }
